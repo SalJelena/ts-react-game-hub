d40e1f50fa2f52b14b8f0e6439dd636c44cccb81
SalJelena <salopekjelena24@gmail.com>
2023-04-09 17:53:43 +0200

    edit styles on genres and add useData hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,19 +1,29 @@
 import { Text, SimpleGrid } from "@chakra-ui/react"
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-    const {games, error} = useGames()
+    const {data, error, isLoading} = useGames();
+    const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
         {error && <Text>{error}</Text>}
         <SimpleGrid 
             columns={{ sm: 1, md: 2, lg: 3, xl: 5}} 
-            spacing={10}
-            padding={10}
+            spacing={3}
+            padding='10px'
         >
-            {games.map(game => <GameCard key={game.id} game={game} />)}
+            {isLoading && skeletons.map(skeleton => <GameCardContainer key={skeleton}>
+                    <GameCardSkeleton />
+                </GameCardContainer> 
+            )}
+            {data.map(game => <GameCardContainer key={game.id}>
+                    <GameCard game={game} />
+                </GameCardContainer>
+            )}
         </SimpleGrid>
     </>
   );
---
+++ src/components/GenreList.tsx
@@ -0,0 +1,43 @@
+import {
+  HStack,
+  List,
+  ListItem,
+  Image,
+  Text,
+  Spinner,
+  Button,
+} from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
+
+const GenreList = () => {
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
+
+  return (
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Button
+              onClick={() => console.log(genre)}
+              fontSize="lg"
+              variant="link"
+            >
+              {genre.name}
+            </Button>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default GenreList;
