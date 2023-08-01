e3567b56f8f5bf94903c0f950d9bcd71b8e5c2cf
SalJelena <salopekjelena24@gmail.com>
2023-08-01 14:04:57 +0200

    import react query and start refactoring

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,7 @@
-import useData from "./useData";
-
+import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import useData, { FetchResponse } from "./useData";
+import { useQuery } from "@tanstack/react-query";
 
 interface Platform {
     id: number;
@@ -7,6 +9,14 @@ interface Platform {
     slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+const usePlatforms = () => useQuery({
+    queryKey: ['platforms'],
+    queryFn: () => 
+        apiClient
+            .get<FetchResponse<Platform>>('/platforms/lists/parents')
+            .then(res => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: platforms.length, results: platforms }
+})
 
 export default usePlatforms;
