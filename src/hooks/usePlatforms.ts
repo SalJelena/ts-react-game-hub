1e2cb5728a8dcfff98458c8e5dc41d80aae7e782
SalJelena <salopekjelena24@gmail.com>
2023-08-02 12:39:32 +0200

    create lookup hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,5 @@
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client"; 
 import { useQuery } from "@tanstack/react-query";
 
 export interface Platform {
@@ -15,7 +14,7 @@ const usePlatforms = () => useQuery({
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
-    initialData: { count: platforms.length, results: platforms }
+    initialData: platforms
 })
 
 export default usePlatforms;
