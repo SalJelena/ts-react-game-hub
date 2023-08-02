7b837154a2ae8adc020ce62ec3c7f5c1a2955a7b
SalJelena <salopekjelena24@gmail.com>
2023-08-02 17:23:18 +0200

    edit game details page and router

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
 import getCroppedImageUrl from '../services/image-url'
 import Emoji from './Emoji'
+import { Link } from "react-router-dom"
 
 interface Props {
     game: Game
@@ -12,14 +13,18 @@ interface Props {
 const GameCard = ({game}: Props) => {
 
   return (
-    <Card width='100%' borderRadius={10} overflow='hidden'>
+    <Card width='100%' borderRadius={10} overflow='hidden' >
         <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
             <HStack justifyContent='space-between' marginBottom={3}>
                 <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
                 <CriticScore score={game.metacritic} />
             </HStack>
-            <Heading fontSize='2xl'>{game.name}<Emoji rating={game.rating_top} /></Heading>
+            <Heading fontSize='2xl'>
+              <Link to={'/games/' + game.slug}>
+                {game.name}
+              </Link>
+              <Emoji rating={game.rating_top} /></Heading>
         </CardBody>
     </Card>
   )
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -8,7 +8,9 @@ import useGameQueryStore from "../store";
 export interface Game {
   id: number;
   name: string;
+  slug: string;
   background_image: string;
+  description_raw: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
@@ -41,8 +43,6 @@ const useGames = () => {
   })
 }
 
-
-  
 
   // useData<Game>(
   //   "/games",
